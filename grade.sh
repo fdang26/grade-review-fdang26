92b95593a55868f9655c0173494a58f87a0cae4d
fdang26 <96316038+fdang26@users.noreply.github.com>
2023-03-01 13:43:27 -0800

    Updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,20 +1,22 @@
 CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 
-score=0
+score="Fail"
 # removes the previous repo
 rm -rf student-submission
 # clones the new submission provided in the first argument
-git clone $1 student-submission
+git clone -q $1 student-submission
 echo 'Finished cloning'
 
 # Check if file exists in new cloned repo
 if [[ ! -f student-submission/ListExamples.java ]]
 then
-    echo "ListExamples.java not found."
+    echo "ListExamples.java not found"
     echo "Score is" $score
     exit
 else
     echo "ListExamples.java found"
+    # get the student code and your test .java file 
+    # into the same directory
     cp TestListExamples.java student-submission/
 fi
 # Move into the cloned repo
@@ -31,5 +33,15 @@ else
     exit
 fi
 # Run the tester on the implementation
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > result.txt
+
+grep -i "Failure" result.txt > failure_ct.txt
+cmd=$(wc -l failure_ct.txt | awk '{print $1}')
+
+if [[ ! $cmd -gt 0 ]]
+then
+    score="Pass"
+fi
+
+echo $score
 
